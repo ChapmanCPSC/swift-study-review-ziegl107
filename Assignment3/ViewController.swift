4650913345b9003a66ccc0808118c255b42492b7
Kayla Ziegler <ziegl107@mail.chapman.edu>
2016-03-29 14:29:33 -0700

    Initial Commit

---
+++ Assignment3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Kayla Ziegler on 3/29/16.
+//  Copyright © 2016 Kayla Ziegler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
